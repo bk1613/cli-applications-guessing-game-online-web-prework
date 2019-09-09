b24d1c94d5f1c2ae58bdda1cb0afa1e5306b6fe7
Brian Kao <bk1613@nyu.edu>
2019-09-09 18:39:31 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,8 +7,10 @@ def run_guessing_game
   number_input = gets.chomp
   if num == number_input
     puts "You guessed the correct number!"
-  elsif 
+  elsif num != number_input
     "Sorry! The computer guessed #{num}."
+    
+    elsif 
   end
 end
 
