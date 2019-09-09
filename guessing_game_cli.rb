c6575323996a19f9ff0a7154063fbe3645cba5f0
Brian Kao <bk1613@nyu.edu>
2019-09-09 18:23:48 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,8 @@ require_relative "../guessing_game_cli"
 def run_guessing_game
   num = rand(1..6)
   "Guess the number between 1 and 6"
-  
+  number_input = gets.chomp
+  if 
 end
 
 run_guessing_game
