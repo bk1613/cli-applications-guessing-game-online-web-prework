0ca945199ff7238a3321ce99729282baacd25ea7
Brian Kao <bk1613@nyu.edu>
2019-09-09 18:52:43 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,17 +1,15 @@
 # Code your solution here!
 def run_guessing_game
-  #generate and store a random number between 1 and 6
+  
   random_number = rand(1..6)
-  #prompt the user to guess their own number between 1 and 6
   puts "Guess your own number between 1 and 6!"
-  #capture user input from the command line 
-  input = gets.chomp
-  #compare that input to the random number that has been generated
-   if input == "exit"
+  num_input = gets.chomp
+  
+   if num_input == "exit"
      puts "Goodbye!"
-   elsif input == random_number
+   elsif num_input == random_number
      puts "You guessed the correct number!"
-   else input != random_number
+   else num_input != random_number
      puts "Sorry! The computer guessed #{random_number}."
    end
   #print out one of three statements
